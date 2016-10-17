067668aba2809ff9df787067faee312de4043e95
levanhung1002 <killer100296@gmail.com>
2016-10-17 22:56:23 +0700

    Update NPC.java

--- src/oop/nhom2/simplerpg/NPC.java
+++ src/oop/nhom2/simplerpg/NPC.java
@@ -3,19 +3,38 @@ package oop.nhom2.simplerpg;
 import java.awt.Point;
 
 public class NPC extends Character {
-
+	private boolean hostile;
+	private String[] dialog;
 	
-
+	NPC(){
+		super();
+	}
+	
+	public void setHostile(boolean hostile){
+		this.hostile = hostile;
+	}
+	
+	public boolean getHostile(){
+		returm this.hostile;
+	}
+	public void setDialog(String[] dialog){
+		this.dialog = dialog.clone();
+	}
+	
+	public String[] getDialog(){
+		return dialog;
+	}
+	
 	@Override
-	public void attack() {
-		// TODO Auto-generated method stub
+	public void attack(){
 		
 	}
-
+	
 	@Override
-	public Point move(int direction) {
-		// TODO Auto-generated method stub
+	public Point move(int direction){
+		
 		return null;
 	}
 
 }
+
